f87f383be7fb08ee4d9b29c0cbe0492a14a30a7f
yuttei1234 <yuchin1234@gmail.com>
2021-08-03 13:11:29 +0000

    [update]index.html.erb内の変数変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,18 @@
 class BooksController < ApplicationController
 
-    def top
-    end
-
     def index
     @books = Book.all
+    @book = Book.new
     end
 
     def show
     @book = Book.find(params[:id])
     end
 
-    def new
-    @book = Book.new
-    end
-
     def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
     if @book.save
-     redirect_to book_path(book.id), notice: "Book was successfully created."
+     redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
      render :index
     end
@@ -28,24 +21,23 @@ class BooksController < ApplicationController
 
     def edit
     @book = Book.find(params[:id])
-    book.save
     if @book.save
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
     else
      render :show
     end
     end
- 
+
     def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "Book was successfully updated."
     end
 
     def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path, notice: "Book was successfully destroyed."
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
     end
 
     private
@@ -53,5 +45,4 @@ class BooksController < ApplicationController
     def book_params
     params.permit(:title, :body)
     end
-
 end
